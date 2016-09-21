bfd7b27524d476b3ad728dbeae105b9ce2c5b948
jitender-axovel <jitender.axovel@gmail.com>
2016-09-21 20:42:27 +0530

    renamed folder and changed structure of html

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,110 @@
+@extends('admin.layouts.adminapp')
+@section('title', $pageTitle)
+@section('content')
+	<h2>Categories</h2>
+	<hr>
+	@include('notification')
+	<table id="categories_list" class="display">
+		<thead>
+			<tr>
+				<th>Title</th>
+				<th>Icon</th>
+				<th>Created On</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($categories as $category)
+			<tr>
+				<td>{{ $category->title}}</td>
+				<td>
+					<img width="100px" height="100px" src="{{ asset(config('image.upload_image_url').$category->image) }}" class="responsive">
+				</td>
+				<td>{{ date_format(date_create($category->created_at), 'F d, Y') }}</td>
+				<td>
+					<ul class="list-inline">
+						<li>
+							<a href="{{ URL::to('admin/bussiness/category/block/'.$category->id) }}">
+			                    @if($category->is_blocked)
+			                    	<button type="button" class="btn btn-danger" title="Unblock"><i class="fa fa-unlock"></i></button>
+		                    	@else
+		                    		<button type="button" class="btn btn-success" title="Block"><i class="fa fa-ban"></i></button>
+		                		@endif
+		                    </a>
+						</li>
+						<li>
+							<a class="btn btn-warning" href="{{ url('admin/bussiness/category/'.$category->id.'/edit') }}" title="Edit"><i class="fa fa-pencil-square-o"></i></a>
+						</li>
+						<li>
+							<form action="{{ url('admin/bussiness/category/'.$category->id) }}" method="POST" onsubmit="deleteCategory('{{$category->id}}', '{{$category->title}}', event,this)">
+								{{csrf_field()}}
+								{{ method_field('DELETE') }}
+								<button type="submit" class="btn btn-danger" title="Delete"><i class="fa fa-trash-o"></i></button>
+							</form>
+						</li>
+					</ul>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+	<script type="text/javascript">
+		$(document).ready( function () {
+		    $('#categories_list').DataTable();
+		} );
+	</script>
+@endsection
+@section('scripts')
+	<script type="text/javascript">
+		function deleteCategory(id, title, event,form)
+		{   
+
+			event.preventDefault();
+			swal({
+				title: "Are you sure?",
+				text: "You want to delete "+title,
+				type: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#DD6B55",
+				confirmButtonText: "Yes, delete it!",
+				cancelButtonText: "No, cancel pls!",
+				closeOnConfirm: false,
+				closeOnCancel: false,
+				allowEscapeKey: false,
+			},
+			function(isConfirm){
+				if(isConfirm) {
+					$.ajax({
+						url: $(form).attr('action'),
+            			data: $(form).serialize(),
+						type: 'DELETE',
+						success: function(data) {
+							data = JSON.parse(data);
+							if(data['status']) {
+								swal({
+									title: data['message'],
+									text: "Press ok to continue",
+									type: "success",
+									showCancelButton: false,
+									confirmButtonColor: "#DD6B55",
+									confirmButtonText: "Ok",
+									closeOnConfirm: false,
+									allowEscapeKey: false,
+								},
+								function(isConfirm){
+									if(isConfirm) {
+										window.location.reload();
+									}
+								});
+							} else {
+								swal("Error", data['message'], "error");
+							}
+						}
+					});
+				} else {
+					swal("Cancelled", title+"'s record will not be deleted.", "error");
+				}
+			});
+		}
+	</script>
+@endsection
