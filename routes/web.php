6eaf04c408aeba914f989f4aae8f65abfded188e
jitender-axovel <jitender.axovel@gmail.com>
2016-09-21 18:51:14 +0530

    decrease padding

--- routes/web.php
+++ routes/web.php
@@ -22,19 +22,19 @@ Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 Route::get('/home', 'HomeController@index');
 
 Route::group(['prefix' => 'admin'], function() {
-		Route::get('login', 'AdminController@login');
-		Route::post('login', 'AdminController@postLogin');
-		Route::group(['middleware' => ['admin']], function () {
-			Route::get('dashboard', 'AdminController@index');
-			Route::get('user/blocked/{id}','AdminUsersController@getBlocked');
-			Route::resource('users', 'AdminUsersController');
-			Route::get('bussiness/category/block/{id}', 'AdminBussinessCategoriesController@getBlocked');
-			Route::resource('bussiness/category', 'AdminBussinessCategoriesController');
-			Route::get('subscription/plan/block/{id}','AdminSubscriptionPlansController@getBlocked');
-			Route::resource('subscription/plan', 'AdminSubscriptionPlansController');
-			Route::get('advertisement/plan/activated/{id}','AdminAdvertisementPlansController@isActivated');
-			Route::resource('advertisement/plan', 'AdminAdvertisementPlansController');
-			Route::get('banner/activated/{id}','AdminBannersController@isActivated');
-			Route::resource('banner', 'AdminBannersController');
-		});	
+	Route::get('login', 'AdminController@login');
+	Route::post('login', 'AdminController@postLogin');
+	Route::group(['middleware' => ['admin']], function () {
+		Route::get('dashboard', 'AdminController@index');
+		Route::get('user/blocked/{id}','AdminUsersController@getBlocked');
+		Route::resource('users', 'AdminUsersController');
+		Route::get('bussiness/category/block/{id}', 'AdminBussinessCategoriesController@getBlocked');
+		Route::resource('bussiness/category', 'AdminBussinessCategoriesController');
+		Route::get('subscription/plan/block/{id}','AdminSubscriptionPlansController@getBlocked');
+		Route::resource('subscription/plan', 'AdminSubscriptionPlansController');
+		Route::get('advertisement/plan/activated/{id}','AdminAdvertisementPlansController@isActivated');
+		Route::resource('advertisement/plan', 'AdminAdvertisementPlansController');
+		Route::get('banner/activated/{id}','AdminBannersController@isActivated');
+		Route::resource('banner', 'AdminBannersController');
+	});
 });
