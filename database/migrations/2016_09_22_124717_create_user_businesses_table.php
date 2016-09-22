327bf21f6b2c7ebe49ddcef12102f90b29c66937
jitender-axovel <jitender.axovel@gmail.com>
2016-09-22 20:21:32 +0530

    added user businesses table and model

---
+++ database/migrations/2016_09_22_124717_create_user_businesses_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserBusinessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_businesses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('bussiness_category_id')->unsigned();
+            $table->foreign('bussiness_category_id')->references('id')->on('bussiness_categories');
+            $table->string('title');
+            $table->string('keywords');
+            $table->longText('about_us');
+            $table->text('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->integer('pin_code');
+            $table->integer('phone_number');
+            $table->integer('secondary_phone_number');
+            $table->string('email');
+            $table->string('website');
+            $table->string('working_hours');
+            $table->boolean('is_agree_to_terms');
+            $table->boolean('is_blocked')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_businesses');
+    }
+}
