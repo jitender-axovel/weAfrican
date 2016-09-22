2e08093cb04aebaa0c32a351549cb5ff5a4673d2
jitender-axovel <jitender.axovel@gmail.com>
2016-09-22 20:24:25 +0530

    removed unnecessary links

--- resources/views/admin/includes/leftmenu.blade.php
+++ resources/views/admin/includes/leftmenu.blade.php
@@ -21,26 +21,7 @@
             <a href="{{url('admin/subscription/plan')}}">Subscription Plans</a>
         </li>
         <li>
-            <a href="javascript:;" data-toggle="collapse" data-target="#advertisement" class="collapsed" aria-expanded="false">Advertisement Plans<i class="fa fa-fw fa-caret-down"></i></a>
-            <ul id="advertisement" class="collapse" aria-expanded="false">
-                <li>
-                    <a href="{{url('admin/advertisement/plan/create')}}">Create New Advertisment</a>
-                </li>
-                <li>
-                    <a href="{{url('admin/advertisement/plan')}}">List Advertisement Plans</a>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <a href="javascript:;" data-toggle="collapse" data-target="#banner" class="collapsed" aria-expanded="false">Banner<i class="fa fa-fw fa-caret-down"></i></a>
-            <ul id="banner" class="collapse" aria-expanded="false">
-                <li>
-                    <a href="{{url('admin/banner/create')}}">Create New Banner</a>
-                </li>
-                <li>
-                    <a href="{{url('admin/banner')}}">List Banners</a>
-                </li>
-            </ul>
+            <a href="{{url('admin/banner')}}">Banner Management</a>
         </li>
     </ul>
 </div>
