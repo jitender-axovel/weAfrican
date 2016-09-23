e1f7ff045940d3d862203bfa32d7792052986635
jitender-axovel <jitender.axovel@gmail.com>
2016-09-23 17:20:29 +0530

    added new views for cms pages

---
+++ resources/views/admin/cms/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('admin.layouts.adminapp')
+@section('title', $page)
+@section('content')
+	<h2>Edit - {{$cmsPage->title}}</h2>
+	<hr>
+	@include('notification')
+	<div class="panel panel-default">
+		<div class="panel-body">
+			<form action="{{ url('admin/cms/'.$cmsPage->id) }}" method="POST">
+				{{csrf_field()}}
+				<input type="hidden" name="_method" value="put" />
+				<div class="form-group">
+					<textarea class="form-control" name="content" rows="15">{{ $cmsPage->content ? $cmsPage->content : old('content') }}</textarea>
+				</div>
+				<button type="submit" class="btn btn-success btn-block" id="btn-login">Update Content</button>
+				</div>
+			</form>
+		</div>
+	</div>
+@endsection
+@section('scripts')
+	<script src='//cdn.tinymce.com/4/tinymce.min.js'></script>
+	<script>
+		tinymce.init({
+			selector: 'textarea',
+			menu: {
+				view: {title: 'Enter Code', items: 'code'}
+			},
+			plugins: 'code, textpattern, textcolor',
+			toolbar: [
+				'undo redo | styleselect | bold italic | link image | alignleft aligncenter alignright alignjustify | fontselect | forecolor | backcolor'
+			],
+			theme_advanced_fonts: 'Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;AkrutiKndPadmini=Akpdmi-n',
+		});
+	</script>
+@endsection
---
+++ resources/views/admin/cms/index.blade.php
@@ -0,0 +1,36 @@
+@extends('admin.layouts.adminapp')
+@section('title', $page)
+@section('content')
+	<h2>Cms Pages</h2>
+	<hr>
+	@include('notification')
+	<table id="categories_list" class="display">
+		<thead>
+			<tr>
+				<th>Title</th>
+				<th>Created On</th>
+				<th>URL</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($cmsPages as $cmsPage)
+			<tr>
+				<td>{{ $cmsPage->title}}</td>
+				<td>{{ date_format(date_create($cmsPage->created_at), 'F d, Y') }}</td>
+				<td>
+					<a href="{{ url('cms/'.$cmsPage->slug) }}" class="btn btn-success" title="Visit"><i class="fa fa-eye"></i></a>
+				</td>
+				<td>
+					<a class="btn btn-info" href="{{ url('admin/cms/'.$cmsPage->id.'/edit/') }}" title="Edit"><i class="fa fa-pencil"></i></a>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+	<script type="text/javascript">
+		$(document).ready( function () {
+		    $('#categories_list').DataTable();
+		} );
+	</script>
+@endsection
