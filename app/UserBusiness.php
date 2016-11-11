395299048ac3187e7cbcf336d9eac9f3ced92e2b
divyaaxovel <divya.ravish@axovel.in>
2016-11-11 16:08:26 +0530

    business banner upload

--- app/UserBusiness.php
+++ app/UserBusiness.php
@@ -259,17 +259,9 @@ class UserBusiness extends Model
 
         $command = 'ffmpeg -i '.config('image.business_banner_path').$image.' -vf scale='.config('image.large_thumbnail_width').':-1 '.config('image.business_banner_path').'thumbnails/large/'.$image;
         shell_exec($command);
+     
 
-
-            
-        $business = array_intersect_key($input, UserBusiness::$updatable);
-        $business['banner'] = $image;
-      return response()->json(['status' => 'success','response' => 'uploaded']);
-
-        $userbusiness = $this->where('id',$input['businessId'])->update($business);
-
-
-
+        $userbusiness = $this->where('id',$input['businessId'])->update(['banner' => $image]);
           
         if($userbusiness)
 
