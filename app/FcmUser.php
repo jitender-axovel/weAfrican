1c5a806fae7c86451a6532e50d454306bcbcfac3
divya.axovel <divya.ravish@axovel.in>
2016-12-07 15:32:55 +0530

    remove error from fcmUser model

--- app/FcmUser.php
+++ app/FcmUser.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class FcmUser extends Model
 {
-    protected $fillable = ['user_id', 'user_role_id' 'fcm_reg_id'];
+    protected $fillable = ['user_id', 'user_role_id', 'fcm_reg_id'];
 
     protected $updatable = ['user_id' => "", 'fcm_reg_id' => "", 'user_role_id' => ""];
 	
