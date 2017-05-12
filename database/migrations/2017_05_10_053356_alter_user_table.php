e4d52cb013847177c7a75b52f2348c786a4b16d7
divyaaxovel <divya.ravish@axovel.in>
2017-05-12 14:28:25 +0530

    Update routes of otp, check-otp, resend-otp

---
+++ database/migrations/2017_05_10_053356_alter_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('user_name')->after('full_name')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table) {
+            $table->dropColumn('user_name');
+        });
+    }
+}
