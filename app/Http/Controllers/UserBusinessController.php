9e34678f906d759fa543b8fc4be636eb3ae8467a
divyaaxovel <divya.ravish@axovel.in>
2017-05-12 14:49:20 +0530

    Add user_name in validator, On Successfull registeration user will redirect to login page with success message,create a random OTP wherever tries to login, resendotp, checkotp

--- app/Http/Controllers/UserBusinessController.php
+++ app/Http/Controllers/UserBusinessController.php
@@ -13,7 +13,9 @@ use App\User;
 use Auth;
 use Validator;
 use App\Helper;
-use session;
+use Illuminate\Support\Facades\Hash;
+//use session;
+use Session;
 
 class UserBusinessController extends Controller
 {
@@ -24,9 +26,8 @@ class UserBusinessController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->only('index','show', 'edit', 'update', 'destroy');
+        $this->middleware('auth')->only('index', 'show', 'edit', 'update', 'destroy');
         $this->user = new User();
-
     }
 
     /**
@@ -46,10 +47,10 @@ class UserBusinessController extends Controller
      */
     public function create()
     {
-        $pageTitle = "Register Business";
-        $categories = BussinessCategory::where('is_blocked',0)->get();
-        $term = CmsPage::where('slug', 'terms-and-conditions')->first();
-        return view('business.register', compact('categories','pageTitle', 'term'));
+        $pageTitle  = "Register Business";
+        $categories = BussinessCategory::where('is_blocked', 0)->get();
+        $term       = CmsPage::where('slug', 'terms-and-conditions')->first();
+        return view('business.register', compact('categories', 'pageTitle', 'term'));
     }
 
     /**
@@ -66,6 +67,7 @@ class UserBusinessController extends Controller
             'title' => 'required',
             'keywords' =>'required',
             'email' => 'required|email|max:255|unique:user_businesses',
+            'user_name' => 'required|max:255|unique:users',
             'mobile_number' => 'required|numeric|unique:users',
             'pin_code' => 'regex:/\b\d{6}\b/',
             'country' => 'string',
@@ -81,20 +83,19 @@ class UserBusinessController extends Controller
 
         $input = $request->input();
 
-        if(isset($input['is_agree_to_terms']))
+        if (isset($input['is_agree_to_terms'])) {
             $input['is_agree_to_terms'] = 1;
-        else 
+        } else {
             $input['is_agree_to_terms'] = 0;
+        }
  
-        if($input['is_agree_to_terms'] == 1)
-        { 
-            if ($request->hasFile('business_logo') ){
-                if ($request->file('business_logo')->isValid())
-                {
-                    $file = $key = md5(uniqid(rand(), true));
-                    $ext = $request->file('business_logo')->
+        if ($input['is_agree_to_terms'] == 1) {
+            if ($request->hasFile('business_logo')) {
+                if ($request->file('business_logo')->isValid()) {
+                    $file  = $key = md5(uniqid(rand(), true));
+                    $ext   = $request->file('business_logo')->
                         getClientOriginalExtension();
-                    $image = $file.'.'.$ext; 
+                    $image = $file.'.'.$ext;
 
                     $fileName = $request->file('business_logo')->move(config('image.logo_image_path'), $image);
 
@@ -108,9 +109,10 @@ class UserBusinessController extends Controller
                     shell_exec($command);
                 }
             }
-            $user = array_intersect_key($request->input(), User::$updatable);
+            $user                 = array_intersect_key($request->input(), User::$updatable);
             $user['user_role_id'] = 3;
-            $user['password'] = bcrypt($input['mobile_number']);
+            $user['password']     = bcrypt($input['mobile_number']);
+            $user['otp']          = rand(1000, 9999);
             
             $user = User::create($user);
             $user->save();
@@ -119,28 +121,21 @@ class UserBusinessController extends Controller
             $user->save();
             
 
-            $business = array_intersect_key($input, UserBusiness::$updatable);
-            $business['business_id']=substr($input['full_name'],0,3).rand(100,999);
-            $business['user_id'] = $user->id;
-            if(isset($fileName)){
+            $business                = array_intersect_key($input, UserBusiness::$updatable);
+            $business['business_id'] =substr($input['full_name'], 0, 3).rand(100, 999);
+            $business['user_id']     = $user->id;
+            if (isset($fileName)) {
                 $business['business_logo'] = $image;
             }
            
             $business = UserBusiness::create($business);
             $business->save();
             $value = $request->session()->get('key');
-            if($business)
-            {
-                if (Auth::attempt(['full_name' => $request->input('full_name'), 'mobile_number' => $request->input('mobile_number'), 'user_role_id' => 3 ,'password' => $request->input('mobile_number')])) {
-                // Authentication passed...
-                    return redirect()->intended('upload');
-                } else {
-                    $errors = new MessageBag(['email' => ['These credentials do not match our records.']]);
-                    return back()->withErrors($errors)->withInput(Input::except('password'));
-                }
-            }else{
-                return back()->with('error', 'Business could not created successfully.Please try again'); 
-            }    
+            if ($business) {
+                return redirect('login')->with('success', 'You have been successfully registered. Please enter username and password to login!');
+            } else {
+                return back()->with('error', 'Business could not created successfully.Please try again');
+            }
         } else {
             return back()->with('error', 'Please select terms and conditions');
         }
@@ -148,19 +143,51 @@ class UserBusinessController extends Controller
 
     public function otp()
     {
+        Session::put('otp_verify', false);
         $pageTitle = "Otp";
         return view('business.otp', compact('pageTitle'));
     }
 
+
+    public function resendotp()
+    {
+        $user_name = Session::get('username');
+        $password  = Session::get('password');
+        $user      = User::whereUserName($user_name)->first();
+        if ($user && Hash::check($password, $user->password)) {
+            $user->otp = rand(1000, 9999);
+            $user->save();
+            Session::put('otp', $user->otp);
+            //Send OTP SMS to the registered mobile number
+            return redirect('otp')->with('success', 'New OTP has been send to your registerd mobile number');
+        } else {
+            return redirect('login')->with('warning', 'Your login session has been expired, Please try to login again!');
+        }
+    }
     public function checkOtp(Request $request)
     {
-
+        $user_name = Session::get('username');
+        $password  = Session::get('password');
+        if (Session::get('otp')==$request->input('otp')) {
+            if (Auth::attempt(['user_name' => $user_name,'password' => $password])) {
+                if (Auth::check()) {
+                    return redirect()->intended('upload');
+                } else {
+                    return redirect()->intended('logout');
+                }
+                //dd(Session::get('username'));
+            } else {
+                return redirect()->intended('logout');
+            }
+        } else {
+            return redirect('otp')->with('error', 'Please Enter the valid OTP');
+        }
     }
 
     public function uploadForm()
     {
-        $pageTitle = "Upload Document";
-        return view('business.upload', compact('pageTitle'));
+            $pageTitle = "Upload Document";
+            return view('business.upload', compact('pageTitle'));
     }
 
     public function uploadDocument(Request $request)
@@ -175,21 +202,19 @@ class UserBusinessController extends Controller
                          ->withInput();
         }
                  
-        if($request->file('identity_proof')->isValid() && $request->file('business_proof')->isValid())
-        {
+        if ($request->file('identity_proof')->isValid() && $request->file('business_proof')->isValid()) {
             $indentityFile = $key = md5(uniqid(rand(), true));
-            $ext = $request->file('identity_proof')->
+            $ext           = $request->file('identity_proof')->
                 getClientOriginalExtension();
-            $image = $indentityFile.'.'.$ext; 
+            $image         = $indentityFile.'.'.$ext;
             
-            $businessFile = $key = md5(uniqid(rand(), true));
-            $businessExt = $request->file('business_proof')->getClientOriginalExtension();
-            $businessImage = $businessFile.'.'.$businessExt; 
+            $businessFile  = $key = md5(uniqid(rand(), true));
+            $businessExt   = $request->file('business_proof')->getClientOriginalExtension();
+            $businessImage = $businessFile.'.'.$businessExt;
 
             $indentityFileName = $request->file('identity_proof')->move(config('image.document_path'), $image);
-            $BusinessFileName = $request->file('business_proof')->move(config('image.document_path'), $businessImage);
+            $BusinessFileName  = $request->file('business_proof')->move(config('image.document_path'), $businessImage);
         } else {
-
             return back()->with('Error', 'Identity Proof and Bussiness Proof is not uploaded. Please try again');
         }
 
@@ -197,10 +222,10 @@ class UserBusinessController extends Controller
    
         $input = array_intersect_key($input, UserBusiness::$updatable);
 
-        $input['identity_proof'] = $image; 
+        $input['identity_proof'] = $image;
         $input['business_proof'] = $businessImage;
            
-        $business = UserBusiness::where('user_id',Auth::id())->update($input);
+        $business = UserBusiness::where('user_id', Auth::id())->update($input);
 
         return redirect('register-business/'.Auth::id())->with('success', 'Document Uploaded successfully');
     }
@@ -214,7 +239,7 @@ class UserBusinessController extends Controller
     public function show($id)
     {
         $pageTitle = "Business Profile";
-        $business = UserBusiness::where('user_id',Auth::id())->first();
+        $business  = UserBusiness::where('user_id', Auth::id())->first();
         return view('business.profile', compact('business', 'pageTitle'));
     }
 
@@ -226,10 +251,10 @@ class UserBusinessController extends Controller
      */
     public function edit($id)
     {
-        $pageTitle = "Bussiness -Edit";
-        $business = UserBusiness::find($id);
-        $categories = BussinessCategory::where('is_blocked',0)->get();
-        return view('business.edit',compact('pageTitle','business','categories'));
+        $pageTitle  = "Bussiness -Edit";
+        $business   = UserBusiness::find($id);
+        $categories = BussinessCategory::where('is_blocked', 0)->get();
+        return view('business.edit', compact('pageTitle', 'business', 'categories'));
     }
 
     /**
@@ -263,13 +288,12 @@ class UserBusinessController extends Controller
         }
 
         $input = $request->input();
-        if ($request->hasFile('business_logo') ){
-            if ($request->file('business_logo')->isValid())
-            {
-                $file = $key = md5(uniqid(rand(), true));
-                $ext = $request->file('business_logo')->
+        if ($request->hasFile('business_logo')) {
+            if ($request->file('business_logo')->isValid()) {
+                $file  = $key = md5(uniqid(rand(), true));
+                $ext   = $request->file('business_logo')->
                     getClientOriginalExtension();
-                $image = $file.'.'.$ext; 
+                $image = $file.'.'.$ext;
 
                 $fileName = $request->file('business_logo')->move(config('image.logo_image_path'), $image);
 
@@ -283,13 +307,12 @@ class UserBusinessController extends Controller
                 shell_exec($command);
             }
         }
-        if ($request->hasFile('banner') ){
-            if ($request->file('banner')->isValid())
-            {
-                $bannerfile = $key = md5(uniqid(rand(), true));
-                $bannerExt = $request->file('banner')->
+        if ($request->hasFile('banner')) {
+            if ($request->file('banner')->isValid()) {
+                $bannerfile  = $key = md5(uniqid(rand(), true));
+                $bannerExt   = $request->file('banner')->
                     getClientOriginalExtension();
-                $bannerImage = $bannerfile.'.'.$bannerExt; 
+                $bannerImage = $bannerfile.'.'.$bannerExt;
 
                 $bannerFileName = $request->file('banner')->move(config('image.banner_image_path').'business/', $bannerImage);
 
@@ -306,18 +329,18 @@ class UserBusinessController extends Controller
 
         $input = array_intersect_key($input, UserBusiness::$updatable);
 
-        if(isset($fileName)) {
+        if (isset($fileName)) {
             $input['business_logo'] =  $image;
-            $user = UserBusiness::where('id',$id)->update($input);
-        } else if(isset($bannerFileName)){
+            $user                   = UserBusiness::where('id', $id)->update($input);
+        } elseif (isset($bannerFileName)) {
             $input['banner'] =  $bannerImage;
-            $user = UserBusiness::where('id',$id)->update($input);
-        } else if((isset($fileName)) && (isset($bannerFileName))){
+            $user            = UserBusiness::where('id', $id)->update($input);
+        } elseif ((isset($fileName)) && (isset($bannerFileName))) {
             $input['business_logo'] =  $image;
-            $input['banner'] =  $bannerImage;
-            $user = UserBusiness::where('id',$id)->update($input);
+            $input['banner']        =  $bannerImage;
+            $user                   = UserBusiness::where('id', $id)->update($input);
         } else {
-            $user = UserBusiness::where('id',$id)->update($input);
+            $user = UserBusiness::where('id', $id)->update($input);
         }
 
         return redirect('register-business/'.$id)->with('success', 'User Business updated successfully');
