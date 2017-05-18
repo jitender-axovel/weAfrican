d00bf14c9a948e769bfcb5d09c926fe84a172280
divyaaxovel <divya.ravish@axovel.in>
2017-05-18 16:56:16 +0530

    Edit product images

--- resources/views/business-product/edit.blade.php
+++ resources/views/business-product/edit.blade.php
@@ -47,15 +47,11 @@
                         </span>
                         @endif
                     </div>
-                    <label for="email" class="col-md-2  control-label">Product Image</label>
-                    <div class="col-md-4">
-                        <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.$product->image)}}"/>
-                    </div>
                 </div>
                 <div class="form-group">
                     <label for="address" class="col-md-2 control-label">Image</label>
                     <div class="col-md-4">
-                        <input type="file" name="product_image" id="product_image">
+                        <input type="file" name="product_image[]" accept="image/*" onchange="previewImg(this)" id="product_image1">
                         @if($errors->has('product_image'))
                         <span class="help-block">
                         <strong>{{ $errors->first('product_image') }}</strong>
@@ -64,7 +60,87 @@
                     </div>
                     <label for="city" class="col-md-2 control-label">Image Preview</label>
                     <div class="col-md-4">
-                        <img src="#" alt=""  id="preview">
+                        @if(explode('|', $product->image)[0]!="")
+                            <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.explode('|', $product->image)[0])}}" alt=""  id="preview1">
+                        @else
+                            <img src="{{asset('images/no-image.jpg')}}" alt=""  id="preview1">
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="address" class="col-md-2 control-label">Image</label>
+                    <div class="col-md-4">
+                        <input type="file" name="product_image[]" accept="image/*" onchange="previewImg(this)" id="product_image2">
+                        @if($errors->has('product_image'))
+                        <span class="help-block">
+                        <strong>{{ $errors->first('product_image') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                    <label for="city" class="col-md-2 control-label">Image Preview</label>
+                    <div class="col-md-4">
+                        @if(explode('|', $product->image)[1]!="")
+                            <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.explode('|', $product->image)[1])}}" alt=""  id="preview2">
+                        @else
+                            <img src="{{asset('images/no-image.jpg')}}" alt=""  id="preview2">
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="address" class="col-md-2 control-label">Image</label>
+                    <div class="col-md-4">
+                        <input type="file" name="product_image[]" accept="image/*" onchange="previewImg(this)" id="product_image3">
+                        @if($errors->has('product_image'))
+                        <span class="help-block">
+                        <strong>{{ $errors->first('product_image') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                    <label for="city" class="col-md-2 control-label">Image Preview</label>
+                    <div class="col-md-4">
+                        @if(explode('|', $product->image)[2]!="")
+                            <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.explode('|', $product->image)[2])}}" alt=""  id="preview3">
+                        @else
+                            <img src="{{asset('images/no-image.jpg')}}" alt=""  id="preview3">
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="address" class="col-md-2 control-label">Image</label>
+                    <div class="col-md-4">
+                        <input type="file" name="product_image[]" accept="image/*" onchange="previewImg(this)" id="product_image4">
+                        @if($errors->has('product_image'))
+                        <span class="help-block">
+                        <strong>{{ $errors->first('product_image') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                    <label for="city" class="col-md-2 control-label">Image Preview</label>
+                    <div class="col-md-4">
+                        @if(explode('|', $product->image)[3]!="")
+                            <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.explode('|', $product->image)[3])}}" alt=""  id="preview4">
+                        @else
+                            <img src="{{asset('images/no-image.jpg')}}" alt=""  id="preview4">
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="address" class="col-md-2 control-label">Image</label>
+                    <div class="col-md-4">
+                        <input type="file" name="product_image[]" accept="image/*" onchange="previewImg(this)" id="product_image5">
+                        @if($errors->has('product_image'))
+                        <span class="help-block">
+                        <strong>{{ $errors->first('product_image') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                    <label for="city" class="col-md-2 control-label">Image Preview</label>
+                    <div class="col-md-4">
+                        @if(explode('|', $product->image)[4]!="")
+                            <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.explode('|', $product->image)[4])}}" alt=""  id="preview5">
+                        @else
+                            <img src="{{asset('images/no-image.jpg')}}" alt=""  id="preview5">
+                        @endif
                     </div>
                 </div>
                 <div class="form-group">
@@ -81,18 +157,17 @@
 @endsection
 @section('scripts')
 <script type="text/javascript">
-    function readURL(input) {
-      if (input.files && input.files[0]) {
-          var reader = new FileReader();
-          reader.onload = function (e) {
-            $('#preview').attr('src', e.target.result);
-          }
-        reader.readAsDataURL(input.files[0]);
-      }
+    function previewImg(img)
+    {
+        var id = img.id[img.id.length -1];
+        input = "#preview"+img.id[img.id.length -1];
+        if (img.files && img.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#preview'+id).attr('src', e.target.result);
+            }
+            reader.readAsDataURL(img.files[0]);
+        }
     }
-    
-    $("#product_image").change(function(){
-        readURL(this);
-    });
 </script>
 @endsection
