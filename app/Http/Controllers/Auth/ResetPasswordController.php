94b94030b949b8c1170aa98feecd4c48f3468d6d
divya <divya.ravish@axovel.in>
2017-05-23 12:00:16 +0530

    change redirect URL

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -20,6 +20,14 @@ class ResetPasswordController extends Controller
 
     use ResetsPasswords;
 
+     /**
+     * Where to redirect users after password reset.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+
     /**
      * Create a new controller instance.
      *
