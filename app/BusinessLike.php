d61d81e875cc04f4c80df45c1ce53f8f53dbc9d8
divyaaxovel <divya.ravish@axovel.in>
2017-05-24 17:44:29 +0530

    Add user relationship

--- app/BusinessLike.php
+++ app/BusinessLike.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class BusinessLike extends Model
 {
     protected $fillable = ['user_id', 'business_id', 'is_like', 'is_dislike'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
 }
