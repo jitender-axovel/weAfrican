d8a3cf10d2bcf1f199dd42cc41f7ca6c5545be0f
divyaaxovel <divya.ravish@axovel.in>
2017-05-24 17:47:44 +0530

    Add new business follower view

---
+++ resources/views/business-follower/index.blade.php
@@ -0,0 +1,189 @@
+@extends('layouts.app')
+@section('title', $pageTitle)
+@section('content')
+<div class="main-container row register-business">
+    <h5 class="text-left">Business Follower List</h5>
+    <p class="text-left">You can see list of users who had liked, disliked or followed your business.</p> 
+    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+      <div class="panel panel-default">
+        <div class="panel-heading" role="tab" id="headingOne">
+          <h4 class="panel-title">
+            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Business Follower List
+            </a>
+          </h4>
+        </div>
+        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+          <div class="panel-body">
+            <div class="panel panel-default ">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>User Name </th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if(count($followers)>0)
+                            @foreach($followers as $follower)
+                                <tr>
+                                    <td>{{ $follower->user->full_name }}</td>
+                                    <td>{{ $follower->user->email }}</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="2">No data found !</td>
+                            </tr>
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading" role="tab" id="headingTwo">
+          <h4 class="panel-title">
+            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Business Like List
+            </a>
+          </h4>
+        </div>
+        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+          <div class="panel-body">
+            <div class="panel panel-default ">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Event Name</th>
+                            <th>Event Keywords</th>
+                            <th>Orgainzer Name</th>
+                            <th>Event Start Date& time</th>
+                            <th>Event End Date& time</th>
+                            <th>Address</th>
+                            <th>Event Banner</th>
+                            <th>Participants</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if(count($likes)>0)
+                            @foreach($likes as $like)
+                                <tr>
+                                    <td>{{ $like->user->full_name }}</td>
+                                    <td>{{ $like->user->email }}</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="2">No data found !</td>
+                            </tr>
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading" role="tab" id="headingThree">
+          <h4 class="panel-title">
+            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Business Dislike List
+            </a>
+          </h4>
+        </div>
+        <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+          <div class="panel-body">
+                <div class="panel panel-default ">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Event Name</th>
+                            <th>Event Keywords</th>
+                            <th>Orgainzer Name</th>
+                            <th>Event Start Date& time</th>
+                            <th>Event End Date& time</th>
+                            <th>Address</th>
+                            <th>Event Banner</th>
+                            <th>Participants</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if(count($dislikes)>0)
+                            @foreach($dislikes as $dislike)
+                                <tr>
+                                    <td>{{ $dislike->user->full_name }}</td>
+                                    <td>{{ $dislike->user->email }}</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="2">No data found !</td>
+                            </tr>
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+@endsection
+@section('scripts')
+<script type="text/javascript">
+    function deleteEvent(id, title, event,form)
+    {   
+    
+        event.preventDefault();
+        swal({
+            title: "Are you sure?",
+            text: "You want to delete "+title,
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel pls!",
+            closeOnConfirm: false,
+            closeOnCancel: false,
+            allowEscapeKey: false,
+        },
+        function(isConfirm){
+            if(isConfirm) {
+                $.ajax({
+                    url: $(form).attr('action'),
+                    data: $(form).serialize(),
+                    type: 'DELETE',
+                    success: function(data) {
+                        data = JSON.parse(data);
+                        if(data['status']) {
+                            swal({
+                                title: data['message'],
+                                text: "Press ok to continue",
+                                type: "success",
+                                showCancelButton: false,
+                                confirmButtonColor: "#DD6B55",
+                                confirmButtonText: "Ok",
+                                closeOnConfirm: false,
+                                allowEscapeKey: false,
+                            },
+                            function(isConfirm){
+                                if(isConfirm) {
+                                    window.location.reload();
+                                }
+                            });
+                        } else {
+                            swal("Error", data['message'], "error");
+                        }
+                    }
+                });
+            } else {
+                swal("Cancelled", title+"'s record will not be deleted.", "error");
+            }
+        });
+    }
+</script>
+@endsection
