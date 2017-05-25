0b4023cd4f7a2a9eaf4e06602374af140a719c5e
divyaaxovel <divya.ravish@axovel.in>
2017-05-25 13:36:01 +0530

    Add first two categories as Entertainment and Skill Professional

--- database/seeds/BussinessCategoriesTableSeeder.php
+++ database/seeds/BussinessCategoriesTableSeeder.php
@@ -14,8 +14,8 @@ class BussinessCategoriesTableSeeder extends Seeder
         DB::table('bussiness_categories')->delete();
         
         DB::table('bussiness_categories')->insert([
-        	['title' => 'category1', 'slug' => 'category-1', 'description' => 'grrftrtyrtyrtyr','image' => 'cf02abe0ef5b9f8e429b6689ebc6ec7e.jpg','is_blocked' => 0,'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-           ['title' => 'category2', 'slug' => 'category-2', 'description' => 'grrftrtyrtyrtyr','image' => '3d0e787630bf396dd2213b6035224665.jpeg','is_blocked' => 0,'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        	['title' => 'Entertainment', 'slug' => 'entertainment', 'description' => 'Entertainment','image' => 'cf02abe0ef5b9f8e429b6689ebc6ec7e.jpg','is_blocked' => 0,'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+           ['title' => 'Skilled Professional', 'slug' => 'skilled-professional', 'description' => 'Skilled Professional','image' => '3d0e787630bf396dd2213b6035224665.jpeg','is_blocked' => 0,'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
 	        ['title' => 'category3', 'slug' => 'category-3', 'description' => 'grrftrtyrtyrtyr','image' => '36c26a20f7d5cbe7ed1e5d0a591a3770.jpeg','is_blocked' => 0,'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
     	]);
     }
