a325b0e0a852001bd1f797da0968f9d6eba2aa44
divyaaxovel <divya.ravish@axovel.in>
2017-05-26 16:20:23 +0530

    Update country detail function

--- app/Http/Controllers/UserBusinessController.php
+++ app/Http/Controllers/UserBusinessController.php
@@ -10,6 +10,7 @@ use App\BussinessCategory;
 use App\UserBusiness;
 use App\CmsPage;
 use App\User;
+use App\CountryList;
 use Auth;
 use Validator;
 use App\Helper;
@@ -546,41 +547,14 @@ class UserBusinessController extends Controller
     public function countryDetails(Request $request)
     {
         $input = $request->input();
-        if($input['country']!="" and $input['country']!=null)
+        $country = $input['country'];
+        $details = CountryList::whereCountry($country)->first();
+        if($details)
         {
-            $curl = curl_init();
-
-            curl_setopt_array($curl, array(
-              CURLOPT_URL => "https://restcountries.eu/rest/v2/name/".$input['country']."?fullText=true",
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_ENCODING => "",
-              CURLOPT_MAXREDIRS => 10,
-              CURLOPT_TIMEOUT => 30,
-              CURLOPT_SSL_VERIFYPEER => false,
-              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-              CURLOPT_CUSTOMREQUEST => "GET",
-              CURLOPT_HTTPHEADER => array(
-                "cache-control: no-cache",
-              ),
-            ));
-
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-            $http = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-            curl_close($curl);
-            if($http=='404')
-            {
-                return "";
-            }else
-            {
-                if ($err) {
-                  print_r("cURL Error #:" . $err);
-                } else {
-                    $temp = json_decode($response);
-                    print_r(json_encode(array('country_code' => $temp[0]->callingCodes[0], 'currency' => $temp[0]->currencies[0]->code)));
-                }
-            }
+            print_r(json_encode(array('country_code' => $details->country_calling_code, 'currency' => $details->country_currency_code)));
+        }else
+        {
+            return "";
         }
     }
 }
