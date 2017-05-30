23a79926965c560c75d2e0769c4ab821c278a51e
divyaaxovel <divya.ravish@axovel.in>
2017-05-30 16:47:23 +0530

    Created migration to create event_seating_plans table

---
+++ database/migrations/2017_05_30_083426_create_event_seating_plans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventSeatingPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_seating_plans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->unique();
+            $table->string('slug');
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('event_seating_plans');
+    }
+}
