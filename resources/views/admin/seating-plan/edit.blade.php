2e75c2376c3c7e7776042403def8007135d1245c
divyaaxovel <divya.ravish@axovel.in>
2017-05-30 16:46:29 +0530

    Created Views to create edit and list the seating plans

---
+++ resources/views/admin/seating-plan/edit.blade.php
@@ -0,0 +1,68 @@
+@extends('admin.layouts.adminapp')
+@section('title', $pageTitle)
+@section('content')
+	<h2>Edit Seating Plan</h2>
+	<hr>
+	@include('notification')
+	<div class="panel panel-default">
+		<div class="panel-body">
+			<form id="seatingplan-form" action="{{ url('admin/seating-plan/'.$seatingplan->id) }}" method="POST" class="form-horizontal" enctype='multipart/form-data'>
+				{{csrf_field()}}
+				{{ method_field('PUT') }}
+				<div class="form-group">
+					<label class="control-label col-md-2">Seating Plan Name</label>
+					<div class="col-md-4">
+					<input type="text" class="form-control" name="title" value="{{ $seatingplan->title or old('title') }}" required>
+						@if($errors->has('title'))
+						<span class="help-block">
+							<strong>{{ $errors->first('title') }}</strong>
+						</span>
+						@endif
+					</div>
+					<label class="control-label col-md-2">Confirm Seating Plan Name</label>
+					<div class="col-md-4">
+						<input type="text" class="form-control" name="confirm_title" required >
+						@if($errors->has('confirm_title'))
+						<span class="help-block">
+							<strong>{{ $errors->first('confirm_title') }}</strong>
+						</span>
+						@endif
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="control-label col-md-2">Description</label>
+					<div class="col-md-10">
+						<textarea required class="form-control" name="description" >{{ $seatingplan->description or old('description') }}</textarea>
+						@if($errors->has('description'))
+							<span class="help-block">
+								<strong>{{ $errors->first('description') }}</strong>
+							</span>
+						@endif
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-md-4 col-md-offset-1">
+						<button type="submit" class="btn btn-success" id="btn-login">Update Seating Plan</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+@endsection
+@section('scripts')
+	<script type="text/javascript">
+		function readURL(input) {
+			if (input.files && input.files[0]) {
+				var reader = new FileReader();
+				reader.onload = function (e) {
+					$('#preview').attr('src', e.target.result);
+				}
+				reader.readAsDataURL(input.files[0]);
+			}
+		}
+
+		$("#category_image").change(function(){
+			readURL(this);
+		});
+	</script>
+@endsection
---
+++ resources/views/admin/seating-plan/index.blade.php
@@ -0,0 +1,99 @@
+@extends('admin.layouts.adminapp')
+@section('title', $pageTitle)
+@section('content')
+	<h2>Event Seating Plan List</h2>
+	<hr>
+	@include('notification')
+	<table id="categories_list" class="display table">
+		<thead>
+			<tr>
+				<th>Title</th>
+				<th>Description</th>
+				<th>Created On</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($seatingplans as $seatingplan)
+			<tr>
+				<td>{{ $seatingplan->title}}</td>
+				<td>{{ $seatingplan->description}}</td>
+				<td>{{ date_format(date_create($seatingplan->created_at), 'd M,Y') }}</td>
+				<td>
+					<ul class="list-inline">
+						<li>
+							<a class="btn btn-warning" href="{{ url('admin/seating-plan/'.$seatingplan->id.'/edit') }}" title="Edit"><i class="fa fa-pencil-square-o"></i></a>
+						</li>
+						<li>
+							<form action="{{ url('admin/seating-plan/'.$seatingplan->id) }}" method="POST" onsubmit="deleteCategory('{{$seatingplan->id}}', '{{$seatingplan->title}}', event,this)">
+								{{csrf_field()}}
+								{{ method_field('DELETE') }}
+								<button type="submit" class="btn btn-danger" title="Delete"><i class="fa fa-trash-o"></i></button>
+							</form>
+						</li>
+					</ul>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+	<script type="text/javascript">
+		$(document).ready( function () {
+		    $('#categories_list').DataTable();
+		} );
+	</script>
+@endsection
+@section('scripts')
+	<script type="text/javascript">
+		function deleteCategory(id, title, event,form)
+		{   
+
+			event.preventDefault();
+			swal({
+				title: "Are you sure?",
+				text: "You want to delete "+title,
+				type: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#DD6B55",
+				confirmButtonText: "Yes, delete it!",
+				cancelButtonText: "No, cancel pls!",
+				closeOnConfirm: false,
+				closeOnCancel: false,
+				allowEscapeKey: false,
+			},
+			function(isConfirm){
+				if(isConfirm) {
+					$.ajax({
+						url: $(form).attr('action'),
+            			data: $(form).serialize(),
+						type: 'DELETE',
+						success: function(data) {
+							data = JSON.parse(data);
+							if(data['status']) {
+								swal({
+									title: data['message'],
+									text: "Press ok to continue",
+									type: "success",
+									showCancelButton: false,
+									confirmButtonColor: "#DD6B55",
+									confirmButtonText: "Ok",
+									closeOnConfirm: false,
+									allowEscapeKey: false,
+								},
+								function(isConfirm){
+									if(isConfirm) {
+										window.location.reload();
+									}
+								});
+							} else {
+								swal("Error", data['message'], "error");
+							}
+						}
+					});
+				} else {
+					swal("Cancelled", title+"'s record will not be deleted.", "error");
+				}
+			});
+		}
+	</script>
+@endsection
