48dfc4cb21e598df99121cee3f857f0c124fa5a8
akashy0752016 <akashy075@gmail.com>
2017-05-31 19:06:03 +0530

    Migration for Securit Question Table

---
+++ database/migrations/2017_05_31_125126_create_security_questions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSecurityQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('security_questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question')->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('security_questions');
+    }
+}
