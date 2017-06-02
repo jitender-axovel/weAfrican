27dc1abc4750300257686bf8503fb66940a8efc5
divyaaxovel <divya.ravish@axovel.in>
2017-06-02 18:22:47 +0530

    Add new table to manage data for entertainment and skill professional category user

---
+++ database/migrations/2017_06_01_121503_create_user_portfolios_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_portfolios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('business_id')->unsigned();
+            $table->foreign('business_id')->references('id')->on('user_businesses');
+            $table->string('maritial_status')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('acedimic_status')->nullable();
+            $table->string('key_skills')->nullable();
+            $table->string('experience')->nullable();
+            $table->string('height')->nullable();
+            $table->string('hair_type')->nullable();
+            $table->string('skin_color')->nullable();
+            $table->string('hair_color')->nullable();
+            $table->boolean('professional_training')->default(false);
+            $table->string('institute_name')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_portfolios');
+    }
+}
