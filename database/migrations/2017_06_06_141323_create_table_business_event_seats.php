9bf2609077cfc6389aa2f8980cfc459310d5b3d1
akashy0752016 <akashy075@gmail.com>
2017-06-07 21:47:12 +0530

    Add migration to create table business event seats

---
+++ database/migrations/2017_06_06_141323_create_table_business_event_seats.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableBusinessEventSeats extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business_event_seats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('business_id')->unsigned();
+            $table->foreign('business_id')->references('id')->on('user_businesses');
+            $table->integer('business_event_id')->unsigned();
+            $table->foreign('business_event_id')->references('id')->on('business_events')->onDelete('cascade');
+            $table->integer('event_seating_plan_id')->unsigned();
+            $table->foreign('event_seating_plan_id')->references('id')->on('event_seating_plans');
+            $table->integer('total_seat_available')->nullable();
+            $table->integer('seat_buyed')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('business_event_seats');
+    }
+}
