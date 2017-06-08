d2681850716f4ffa8f51b829e99fd2bf4d0a458c
akashy0752016 <akashy075@gmail.com>
2017-06-08 23:10:18 +0530

    Add gender column in the users table

---
+++ database/migrations/2017_06_06_053356_alter_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->integer('mobile_verified')->after('is_verified')->default(0);
+            $table->string('gender')->after('last_name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table) {
+            $table->dropColumn('mobile_verified');
+            $table->dropColumn('gender');
+        });
+    }
+}
