495e286bc87fde7e92b5be37f42cb918ff4fdc39
divyaaxovel <divya.ravish@axovel.in>
2017-06-08 16:23:10 +0530

    Add select 2 css and js

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -9,7 +9,9 @@
 
 <!-- Styles -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ asset('css/select2.min.css') }}" />
 <link href="{{ asset('css/sweetalert.css') }}" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="{{asset('css/style.css')}}">
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/t/dt/dt-1.10.11/datatables.min.css"/>
@@ -19,6 +21,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/select2.min.js') }}"></script>
 <script src="{{ asset('js/sweetalert.min.js') }}" type="text/javascript"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/t/dt/dt-1.10.11/datatables.min.js"></script>
 <script>
