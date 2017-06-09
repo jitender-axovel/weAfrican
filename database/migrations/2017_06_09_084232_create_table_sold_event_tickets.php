a1d83812c4e7bf62c74759ee0205e2f403c98302
akashy0752016 <akashy075@gmail.com>
2017-06-09 23:36:49 +0530

    Add migrtaion for creating sold_event_tickets table

---
+++ database/migrations/2017_06_09_084232_create_table_sold_event_tickets.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableSoldEventTickets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sold_event_tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('business_id')->unsigned();
+            $table->foreign('business_id')->references('id')->on('user_businesses');
+            $table->integer('business_event_id')->unsigned();
+            $table->foreign('business_event_id')->references('id')->on('business_events')->onDelete('cascade');
+            $table->integer('event_seating_plan_id')->unsigned();
+            $table->foreign('event_seating_plan_id')->references('id')->on('event_seating_plans');
+            $table->double('per_ticket_price', 15, 2)->nullable();
+            $table->integer('total_tickets_buyed')->nullable();
+            $table->double('total_tickets_price', 15, 2)->nullable();
+            $table->string('transaction_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sold_event_tickets');
+    }
+}
