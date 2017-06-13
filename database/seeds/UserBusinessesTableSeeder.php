0182123ec7660313189a10805d81e0e4de50abe0
akashy0752016 <akashy075@gmail.com>
2017-06-13 22:50:58 +0530

    Added Seeder to add test records in User Business

--- database/seeds/UserBusinessesTableSeeder.php
+++ database/seeds/UserBusinessesTableSeeder.php
@@ -27,6 +27,34 @@ class UserBusinessesTableSeeder extends Seeder
             'is_agree_to_terms' => 1,
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
+        ],[
+            'id' => 2,
+            'user_id' => 6,
+            'business_id' => 'Aka999',
+            'bussiness_category_id' => 3,
+            'title' => 'Divya',
+            'keywords' => 'Divya',
+            'about_us' => 'Divya',
+            'website' => 'akashaxovel.com',
+            'is_identity_proof_validate' => 1,
+            'is_business_proof_validate' => 1,
+            'is_agree_to_terms' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ],[
+            'id' => 3,
+            'user_id' => 7,
+            'business_id' => 'Juhi999',
+            'bussiness_category_id' => 2,
+            'title' => 'Divya',
+            'keywords' => 'Divya',
+            'about_us' => 'Divya',
+            'website' => 'juhiaxovel.com',
+            'is_identity_proof_validate' => 1,
+            'is_business_proof_validate' => 1,
+            'is_agree_to_terms' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
         ]]);
     }
 }
