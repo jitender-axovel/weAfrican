1c57118dfd8ef601845a89b32bd069de0027d5ea
divyaaxovel <divya.ravish@axovel.in>
2017-06-13 11:40:45 +0530

    Add change footer style

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,23 +1,26 @@
 <footer>
 	<div class="row">
 		<div class="container">
-			<div class="col-md-6 text-left col-sm-6 col-xs-12">
+			<div class="col-md-8 text-left col-sm-6 col-xs-12">
 				<ul class="footer-linkss list-inline">
 					@foreach($cmsPages as $cms)
 					<li> <a href="{{url('cms/'.$cms->slug)}}">{{$cms->title}}</a> </li>
 					@endforeach
 			 	</ul>
 			</div>
-			<div class="col-md-6 text-right col-sm-6 col-xs-12">
-				<p>2016 - {{ date('Y')}} © Copyright your company</p>
-				<p>All Rights Reserved</p>
-				<div class="available">
-					<p>available on</p>
+			<div class="col-md-4 text-right col-sm-6 col-xs-12">
+				<div>
+					<!-- <p>available on</p> -->
 					<ul class="list-inline">
 						<li> <a href="#"><img src="{{asset('images/apple-store.png')}}"></a> </li>
 					    <li> <a href="#"><img src="{{asset('images/play-store.png')}}"></a> </li>
 					</ul>
 				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="col-md-12 text-left col-sm-12 col-xs-12">
+				<p>2016 - {{ date('Y')}} © Copyright your company. All Rights Reserved</p>
 			</div>
 		</div>
 	</div>	
