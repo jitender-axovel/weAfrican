3e86c5d162f6543b321496babb91a29921da0144
AkashyadavAxovel <divya.ravish@axovel.in>
2017-06-16 23:28:37 +0530

    add new code

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -1,14 +1,14 @@
 <header class="admin-header">
     <div class="container-fluid">
-        <div class="col-md-6 left-side">
+        <div class="col-md-6 col-xs-6 left-side">
             <ul class="left-menu list-inline">
                 <li><a href="#" class="site-logo"><img src="{{asset('images/admin_logo.png')}}"></a></li>
             </ul>
         </div>
         <div class="col-md-6 right-side">
             <ul class="list-inline">
-                <li class="login"><a class="btn btn-success" href="{{url('/')}}" target="_blank">Visit Site</a></li>
-                <li><a class="btn btn-danger" href="{{ url('logout') }}"><i class="fa fa-power-off"></i> Logout</a></li>
+                <li class="login"><a class="btn btn-success top_btn" href="{{url('/')}}" target="_blank">Visit Site</a></li>
+                <li><a class="btn btn-danger top_btn" href="{{ url('logout') }}"><i class="fa fa-power-off"></i> Logout</a></li>
             </ul>
         </div>
     </div>
