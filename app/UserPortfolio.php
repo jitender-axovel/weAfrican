d5c5e8f38a2c886264828bc9432ad10c3facdf1e
divyaaxovel <divya.ravish@axovel.in>
2017-06-19 16:39:44 +0530

    Remove featured image and image from updateable and fillable

--- app/UserPortfolio.php
+++ app/UserPortfolio.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserPortfolio extends Model
 {
-     protected $fillable = ['user_id', 'business_id', 'maritial_status', 'occupation', 'acedimic_status' ,'key_skills', 'experience_years', 'experience_months', 'height_feets', 'height_inches' , 'hair_type', 'skin_color' , 'hair_color' , 'professional_training', 'institute_name', 'image', 'featured_image'];
+     protected $fillable = ['user_id', 'business_id', 'maritial_status', 'occupation', 'acedimic_status' ,'key_skills', 'experience_years', 'experience_months', 'height_feets', 'height_inches' , 'hair_type', 'skin_color' , 'hair_color' , 'professional_training', 'institute_name'];
 
-    public static $updatable = ['user_id' => "", 'business_id' => "", 'maritial_status' => "", 'occupation' => "", 'acedimic_status' => "",'key_skills' => "", 'experience_years' => "", 'experience_months' => "", 'height_feets' => "", 'height_inches' => "", 'hair_type' => "", 'skin_color' => "", 'hair_color' => "", 'professional_training' => "", 'institute_name' => "", 'image' => "", 'featured_image' => ""];
+    public static $updatable = ['user_id' => "", 'business_id' => "", 'maritial_status' => "", 'occupation' => "", 'acedimic_status' => "",'key_skills' => "", 'experience_years' => "", 'experience_months' => "", 'height_feets' => "", 'height_inches' => "", 'hair_type' => "", 'skin_color' => "", 'hair_color' => "", 'professional_training' => "", 'institute_name' => ""];
 
     public function portfolio_images()
     {
