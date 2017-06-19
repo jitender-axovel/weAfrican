eedf43b92689f52dcf25f58405fb05fc11c5f47d
AkashyadavAxovel <divya.ravish@axovel.in>
2017-06-19 17:49:36 +0530

    add new class for some changes and make responsive

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
 	<div class="row">
 		<div class="container">
-			<div class="col-md-8 text-left col-sm-6 col-xs-12">
+			<div class="col-md-8 text-left col-sm-6 col-xs-12 all_about">
 				<ul class="footer-linkss list-inline">
 					@foreach($cmsPages as $cms)
 					<li> <a href="{{url('cms/'.$cms->slug)}}">{{$cms->title}}</a> </li>
@@ -11,7 +11,7 @@
 			<div class="col-md-4 text-right col-sm-6 col-xs-12">
 				<div>
 					<!-- <p>available on</p> -->
-					<ul class="list-inline">
+					<ul class="list-inline image_sep">
 						<li> <a href="#"><img src="{{asset('images/apple-store.png')}}"></a> </li>
 					    <li> <a href="#"><img src="{{asset('images/play-store.png')}}"></a> </li>
 					</ul>
