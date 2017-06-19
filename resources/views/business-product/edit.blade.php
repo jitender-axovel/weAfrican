85aa1c6f70ea8727db5d7de1f44114869db8d603
divyaaxovel <divya.ravish@axovel.in>
2017-06-19 16:26:01 +0530

    Added add more button to add images and validation on add more and select featured image

--- resources/views/business-product/edit.blade.php
+++ resources/views/business-product/edit.blade.php
@@ -49,99 +49,81 @@
                         @endif
                     </div>
                 </div>
-                <div class="form-group">
-                    <label for="address" class="col-md-2 control-label">Image</label>
-                    <div class="col-md-4">
-                        <input type="file" name="product_image[]" accept="image/*" onchange="previewImg(this)" id="product_image1">
-                        @if($errors->has('product_image'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('product_image') }}</strong>
-                        </span>
-                        @endif
+                @if(count($product_images)>0)
+                @foreach($product_images as $key => $product_image)
+                    @if($key==0)
+                        <div class="form-group">
+                            <input type="hidden" class="product_image_id" name="product_image_id[]" value="{{$product_image->id}}">
+                            <label class="col-xs-2 control-label">Image</label>
+                            <div class="col-xs-3">
+                                <input type="file" class="form-control product_image" accept="image/*" name="product_image[{{$product_image->id}}]" value="{{ asset(config('image.product_image_url').'thumbnails/small/'.$product_image->image) }}" onchange="previewImg(this)" />
+                            </div>
+                            <div class="col-xs-3">
+                                <img src="{{ asset(config('image.product_image_url').'thumbnails/small/'.$product_image->image) }}" alt="" id="preview">
+                            </div>
+                            <div class="col-xs-3">
+                                <div class="radio">
+                                  <label><input type="radio" class="featured_image" name="featured_image" @if($product_image->featured_image==1) checked="checked" @endif value="{{$key+1}}" /><br>Featured Image</label>
+                                </div>
+                            </div>
+                            <div class="col-xs-1">
+                                <button type="button" class="btn btn-default addButton"><i class="fa fa-plus"></i></button>
+                            </div>
+                        </div>
+                    @else
+                        <div class="form-group">
+                            <input type="hidden" class="product_image_id" name="product_image_id[]" value="{{$product_image->id}}">
+                            <div class="col-xs-3 col-xs-offset-2">
+                                <input type="file" class="form-control product_image" accept="image/*" name="product_image[{{$product_image->id}}]" value="{{ asset(config('image.product_image_url').'thumbnails/small/'.$product_image->image) }}" onchange="previewImg(this)" />
+                            </div>
+                            <div class="col-xs-3">
+                                <img src="{{ asset(config('image.product_image_url').'thumbnails/small/'.$product_image->image) }}" alt="" id="preview">
+                            </div>
+                            <div class="col-xs-3">
+                                <div class="radio">
+                                  <label><input type="radio" class="featured_image" name="featured_image" @if($product_image->featured_image==1) checked="checked" @endif value="{{$key+1}}" /><br>Featured Image</label>
+                                </div>
+                            </div>
+                            <div class="col-xs-1">
+                                <button type="button" class="btn btn-default removeButton"><i class="fa fa-minus"></i></button>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+                @else
+                    <div class="form-group">
+                        <label class="col-xs-2 control-label">Image</label>
+                        <div class="col-xs-3">
+                            <input type="file" class="form-control" accept="image/*" name="product_image[]" onchange="previewImg(this)" />
+                        </div>
+                        <div class="col-xs-3">
+                            <img src="{{asset('images/no-image.jpg')}}" alt="" id="preview">
+                        </div>
+                        <div class="col-xs-3">
+                            <div class="radio">
+                              <label><input type="radio" class="featured_image" name="featured_image" checked="checked" value="1" />&nbsp;Featured Image</label>
+                            </div>
+                        </div>
+                        <div class="col-xs-1">
+                            <button type="button" class="btn btn-default addButton"><i class="fa fa-plus"></i></button>
+                        </div>
                     </div>
-                    <label for="city" class="col-md-2 control-label">Image Preview</label>
-                    <div class="col-md-4">
-                        @if(explode('|', $product->image)[0]!="")
-                            <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.explode('|', $product->image)[0])}}" alt=""  id="preview1">
-                        @else
-                            <img src="{{asset('images/no-image.jpg')}}" alt=""  id="preview1">
-                        @endif
+                @endif
+                <!-- The template for adding new field -->
+                <div class="form-group hide" id="bookTemplate">
+                    <div class="col-xs-3 col-xs-offset-2">
+                        <input type="file" class="form-control product_image" accept="image/*" name="product_image[]" onchange="previewImg(this)" />
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="address" class="col-md-2 control-label">Image</label>
-                    <div class="col-md-4">
-                        <input type="file" name="product_image[]" accept="image/*" onchange="previewImg(this)" id="product_image2">
-                        @if($errors->has('product_image'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('product_image') }}</strong>
-                        </span>
-                        @endif
+                    <div class="col-xs-3">
+                        <img src="{{asset('images/no-image.jpg')}}" alt="" id="preview">
                     </div>
-                    <label for="city" class="col-md-2 control-label">Image Preview</label>
-                    <div class="col-md-4">
-                        @if(explode('|', $product->image)[1]!="")
-                            <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.explode('|', $product->image)[1])}}" alt=""  id="preview2">
-                        @else
-                            <img src="{{asset('images/no-image.jpg')}}" alt=""  id="preview2">
-                        @endif
+                    <div class="col-xs-3">
+                        <div class="radio">
+                          <label><input type="radio" class="featured_image" name="featured_image" value="1" /><br>Featured Image</label>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="address" class="col-md-2 control-label">Image</label>
-                    <div class="col-md-4">
-                        <input type="file" name="product_image[]" accept="image/*" onchange="previewImg(this)" id="product_image3">
-                        @if($errors->has('product_image'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('product_image') }}</strong>
-                        </span>
-                        @endif
-                    </div>
-                    <label for="city" class="col-md-2 control-label">Image Preview</label>
-                    <div class="col-md-4">
-                        @if(explode('|', $product->image)[2]!="")
-                            <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.explode('|', $product->image)[2])}}" alt=""  id="preview3">
-                        @else
-                            <img src="{{asset('images/no-image.jpg')}}" alt=""  id="preview3">
-                        @endif
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="address" class="col-md-2 control-label">Image</label>
-                    <div class="col-md-4">
-                        <input type="file" name="product_image[]" accept="image/*" onchange="previewImg(this)" id="product_image4">
-                        @if($errors->has('product_image'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('product_image') }}</strong>
-                        </span>
-                        @endif
-                    </div>
-                    <label for="city" class="col-md-2 control-label">Image Preview</label>
-                    <div class="col-md-4">
-                        @if(explode('|', $product->image)[3]!="")
-                            <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.explode('|', $product->image)[3])}}" alt=""  id="preview4">
-                        @else
-                            <img src="{{asset('images/no-image.jpg')}}" alt=""  id="preview4">
-                        @endif
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="address" class="col-md-2 control-label">Image</label>
-                    <div class="col-md-4">
-                        <input type="file" name="product_image[]" accept="image/*" onchange="previewImg(this)" id="product_image5">
-                        @if($errors->has('product_image'))
-                        <span class="help-block">
-                        <strong>{{ $errors->first('product_image') }}</strong>
-                        </span>
-                        @endif
-                    </div>
-                    <label for="city" class="col-md-2 control-label">Image Preview</label>
-                    <div class="col-md-4">
-                        @if(explode('|', $product->image)[4]!="")
-                            <img src="{{asset(config('image.product_image_url').'thumbnails/small/'.explode('|', $product->image)[4])}}" alt=""  id="preview5">
-                        @else
-                            <img src="{{asset('images/no-image.jpg')}}" alt=""  id="preview5">
-                        @endif
+                    <div class="col-xs-1">
+                        <button type="button" class="btn btn-default removeButton"><i class="fa fa-minus"></i></button>
                     </div>
                 </div>
                 <div class="form-group">
@@ -157,18 +139,181 @@
 </div>
 @endsection
 @section('scripts')
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.formvalidation/0.6.1/css/formValidation.min.css">
+<script src='https://cdn.jsdelivr.net/jquery.formvalidation/0.6.1/js/formValidation.min.js'></script>
+<script src='https://cdn.jsdelivr.net/jquery.formvalidation/0.6.1/js/framework/bootstrap.min.js'></script>
 <script type="text/javascript">
     function previewImg(img)
     {
         var id = img.id[img.id.length -1];
-        input = "#preview"+img.id[img.id.length -1];
         if (img.files && img.files[0]) {
             var reader = new FileReader();
             reader.onload = function (e) {
-                $('#preview'+id).attr('src', e.target.result);
+                $(img).closest('.form-group').find("img").attr('src', e.target.result);
             }
             reader.readAsDataURL(img.files[0]);
         }
     }
+    
+    $(document).ready(function() {
+        // The maximum number of options
+        var MAX_OPTIONS = 5;
+        $('#register-form').formValidation({
+            framework: 'bootstrap',
+            icon: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                title: {
+                    validators: {
+                            stringLength: {
+                            min: 2,
+                        },
+                            notEmpty: {
+                            message: 'Please supply your product title'
+                        }
+                    }
+                },
+                price: {
+                    validators:{
+                        notEmpty: {
+                                message: 'Please supply your product price'
+                            },
+                        numeric: {
+                        message: 'The price must be a number',
+                        transformer: function($field, validatorName, validator) {
+                            var value = $field.val();
+                            return value.replace(',', '');
+                        }
+                    }
+                    }
+                },
+                description: {
+                    validators:{
+                    notEmpty: {
+                            message: 'Please supply your product description'
+                        }
+                    }
+                },
+                'product_image[]': {
+                    row: '.col-xs-3',
+                    validators: {
+                        notEmpty: {
+                            message: 'Please choose a product image'
+                        },
+                        file: {
+                            extension: 'jpeg,jpg,png',
+                            type: 'image/jpg,image/png,image/x-png,image/x-jpg,image/jpeg',
+                            maxSize: 10 * 1024 * 1024, // 2048 * 1024
+                            message: 'Please choose a image file with a size less than 4M.',
+                        },
+                    }
+                },
+                featured_image: {
+                    validators:{
+                    notEmpty: {
+                            message: 'Please select a featured image'
+                        }
+                    }
+                },
+                @foreach($product_images as $product_image)
+                    'product_image[{{$product_image->id}}]':{
+                        row: '.col-xs-3',
+                        validators: {
+                            file: {
+                                extension: 'jpeg,jpg,png',
+                                type: 'image/jpg,image/png,image/x-png,image/x-jpg,image/jpeg',
+                                maxSize: 10 * 1024 * 1024, // 2048 * 1024
+                                message: 'Please choose a image file with a size less than 4M.',
+                            },
+                        }
+                    },
+                @endforeach
+            }
+        })
+
+        // Called after adding new field
+        .on('added.field.fv', function(e, data) {
+            // data.field   --> The field name
+            // data.element --> The new field element
+            // data.options --> The new field options
+            if (data.field === 'product_image[]') {
+                if ($('#register-form').find(':visible[name^="product_image["]').length >= MAX_OPTIONS) {
+                    $('#register-form').find('.addButton').attr('disabled', 'disabled');
+                }
+            }
+        })
+
+        // Called after removing the field
+        .on('removed.field.fv', function(e, data) {
+           if (data.field === 'product_image[]') {
+                if ($('#register-form').find(':visible[name^="product_image["]').length < MAX_OPTIONS) {
+                    $('#register-form').find('.addButton').removeAttr('disabled');
+                }
+            }
+        })
+
+        // Add button click handler
+        .on('click', '.addButton', function() {
+            var $template = $('#bookTemplate'),
+                $clone    = $template
+                                .clone()
+                                .removeClass('hide')
+                                .removeAttr('id')
+                                .insertBefore($template);
+
+            // Add new fields
+            // Note that we DO NOT need to pass the set of validators
+            // because the new field has the same name with the original one
+            // which its validators are already set
+            $("#featured_image option:not(:first)").remove();
+            for(var i=1; i<=$('#register-form').find(':visible[name^="product_image["]').length; i++)
+            {
+                $('#featured_image').append('<option value="'+i+'">'+i+'</option>');
+            }
+            $('#register-form')
+                .formValidation('addField', $clone.find('[name="product_image[]"]'))
+                .formValidation('revalidateField', 'featured_image');
+            var i=1;
+            $('.featured_image').each(function(){
+                if ($('#register-form').find(':visible[name="product_title[]"]').length < MAX_OPTIONS) {
+                    var $row = $(this).closest('.form-group');
+                    $row.find('[name="featured_image"]').attr('value',i);
+                    i++;
+                }
+            });
+            $('#register-form').formValidation('revalidateField', 'featured_image');
+        })
+
+        // Remove button click handler
+        .on('click', '.removeButton', function() {
+            var $row = $(this).closest('.form-group');
+
+            // Remove fields
+            $('#register-form')
+                .formValidation('removeField', $row.find('[name="product_image[]"]'))
+                .formValidation('revalidateField', 'featured_image');
+
+            // Remove element containing the fields
+            $row.remove();
+            var i=1;
+            $('.featured_image').each(function(){
+                if ($('#register-form').find(':visible[name^="product_image["]').length < MAX_OPTIONS) {
+                    var $row = $(this).closest('.form-group');
+                    $row.find('[name="featured_image"]').attr('value',i);
+                    i++;
+                }
+            });
+            $('#register-form').formValidation('revalidateField', 'featured_image');
+        })
+        .on('success.field.fv', function(e, data) {
+            if (data.fv.getSubmitButton()) {
+                data.fv.disableSubmitButtons(false);
+            }
+        });
+        
+    });
 </script>
 @endsection
