d4171030c26cc6f1baa4e6e0a0785ec5abe87760
divyaaxovel <divya.ravish@axovel.in>
2017-06-19 16:21:30 +0530

    Add new relationship for get portfolio images

--- app/UserPortfolio.php
+++ app/UserPortfolio.php
@@ -9,4 +9,9 @@ class UserPortfolio extends Model
      protected $fillable = ['user_id', 'business_id', 'maritial_status', 'occupation', 'acedimic_status' ,'key_skills', 'experience_years', 'experience_months', 'height_feets', 'height_inches' , 'hair_type', 'skin_color' , 'hair_color' , 'professional_training', 'institute_name', 'image', 'featured_image'];
 
     public static $updatable = ['user_id' => "", 'business_id' => "", 'maritial_status' => "", 'occupation' => "", 'acedimic_status' => "",'key_skills' => "", 'experience_years' => "", 'experience_months' => "", 'height_feets' => "", 'height_inches' => "", 'hair_type' => "", 'skin_color' => "", 'hair_color' => "", 'professional_training' => "", 'institute_name' => "", 'image' => "", 'featured_image' => ""];
+
+    public function portfolio_images()
+    {
+        return $this->hasMany('App\UserPortfolioImage','user_portfolio_id');
+    }
 }
