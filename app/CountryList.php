c0e5c8c5cdb0a0f2dc77147d1d15eeb88351c564
divya <divya.ravish@axovel.in>
2017-06-22 10:32:44 +0530

    Created new function to get currency

--- app/CountryList.php
+++ app/CountryList.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class CountryList extends Model
 {
-    //
+    public function apiGetCurrency($countryName)
+    {
+    	$currency = $this->whereCountry($countryName)->first();
+    	return $currency;
+    }
 }
