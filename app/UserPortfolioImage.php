6afe806c764b57b061be2b8b4e84024adf0c1816
AkashyadavAxovel <akash.axovel@gmail.com>
2017-06-26 11:24:33 +0530

    Added new function to add/update user portfolio images, title and description

--- app/UserPortfolioImage.php
+++ app/UserPortfolioImage.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use App\UserBusiness;
 use Validator;
+use Image;
+use File;
 
 class UserPortfolioImage extends Model
 {
@@ -23,5 +25,160 @@ class UserPortfolioImage extends Model
     {
         $portfolioImages = $this->whereuserPortfolioId($input['portfolioId'])->wherebusinessId($input['businessId'])->whereuserId($input['userId'])->get();
         return $portfolioImages;
+    }
+
+    public function apiPostUserPortfolioDetail(Request $request)
+    {
+    	$input = $request->input();
+
+    	$rule = array(
+    		'userId' => 'required',
+            'businessId' => 'required',
+            'portfolioId' => 'required',
+    		'title' => 'required',
+            'description' => 'required',
+            'featured_image' => 'integer'
+    	);
+
+    	if(isset($input['portfolioImageId']))
+    	{
+    		$rule['image'] = 'string';
+    	}else
+    	{
+    		$rule['image'] = 'required|string';
+    	}
+    	
+        $validator = Validator::make($input, $rule);
+
+        if($validator->fails()){
+            if(count($validator->errors()) <= 1){
+                    return response()->json(['status' => 'exception','response' => $validator->errors()->first()]);   
+            } else{
+                return response()->json(['status' => 'exception','response' => 'All fields are required']);   
+            }
+        }
+
+        $input['user_id'] = $input['userId'];
+    	$input['business_id'] = $input['businessId'];
+    	$input['user_portfolio_id'] = $input['portfolioId'];
+
+        if(isset($input['portfolioImageId']))
+        {
+        	$portfolioImage = $this->whereuserId($input['user_id'])->wherebusinessId($input['business_id'])->whereuserPortfolioId($input['user_portfolio_id'])->whereId($input['portfolioImageId'])->first();
+        	if($portfolioImage)
+        	{
+        		if(isset($input['image']) && !empty($input['image']))
+	        	{
+	        		$data = $input['image'];
+
+	                $img = str_replace('data:image/jpeg;base64,', '', $data);
+	                $img = str_replace(' ', '+', $img);
+
+	                $data = base64_decode($img);
+
+	                $fileName = md5(uniqid(rand(), true));
+
+	                $image = $fileName.'.'.'png';
+
+	                $file = config('image.portfolio_image_path').$image;
+
+	                $success = file_put_contents($file, $data);
+
+	                $img = Image::make($file);
+	                    
+	                $img->resize(config('image.large_thumbnail_width'), null, function($constraint) {
+	                             $constraint->aspectRatio();
+	                        })->save(config('image.portfolio_image_path').'/thumbnails/large/'.$image); 
+
+	                $img->resize(config('image.medium_thumbnail_width'), null, function($constraint) {
+	                     $constraint->aspectRatio();
+	                })->save(config('image.portfolio_image_path').'/thumbnails/medium/'.$image);
+	                        
+	                $img->resize(config('image.small_thumbnail_width'), null, function($constraint) {
+	                     $constraint->aspectRatio();
+	                })->save(config('image.portfolio_image_path').'/thumbnails/small/'.$image);
+	        	}
+
+	        	if($portfolioImage->image!="")
+	        	{
+	        		if(File::exists(config('image.portfolio_image_path').$portfolioImage->image))
+	        		{
+	        			File::delete(config('image.portfolio_image_path').$portfolioImage->image);
+
+	        			File::delete(config('image.portfolio_image_path').'/thumbnails/small/'.$portfolioImage->image);
+
+	        			File::delete(config('image.portfolio_image_path').'/thumbnails/medium/'.$portfolioImage->image);
+
+	        			File::delete(config('image.portfolio_image_path').'/thumbnails/large/'.$portfolioImage->image);
+	        		}
+	        	}
+
+	        	if(isset($image))
+	        	{
+	        		$input['image'] = $image;
+	        	}
+
+	        	$portfolioImage = array_intersect_key($input, UserPortfolioImage::$updatable);
+
+	        	$portfolioImage = $this->whereuserId($input['user_id'])->wherebusinessId($input['business_id'])->whereuserPortfolioId($input['user_portfolio_id'])->whereId($input['portfolioImageId'])->update($portfolioImage);
+
+	        	if($portfolioImage)
+	                return response()->json(['status' => 'success','response' => "Portfolio Image has been updated successfully."]);
+	            else
+	                return response()->json(['status' => 'failure','response' => "Portfolio Image could not updated successfully.Please try again."]);
+
+        	}else
+        	{
+        		return response()->json(['status' => 'exception','response' => 'portfolio Image dose not exists']);
+        	}
+        	
+        }else
+        {
+        	if(isset($input['image']) && !empty($input['image']))
+        	{
+        		$data = $input['image'];
+
+                $img = str_replace('data:image/jpeg;base64,', '', $data);
+                $img = str_replace(' ', '+', $img);
+
+                $data = base64_decode($img);
+
+                $fileName = md5(uniqid(rand(), true));
+
+                $image = $fileName.'.'.'png';
+
+                $file = config('image.portfolio_image_path').$image;
+
+                $success = file_put_contents($file, $data);
+
+                $img = Image::make($file);
+                    
+                $img->resize(config('image.large_thumbnail_width'), null, function($constraint) {
+                             $constraint->aspectRatio();
+                        })->save(config('image.portfolio_image_path').'/thumbnails/large/'.$image); 
+
+                $img->resize(config('image.medium_thumbnail_width'), null, function($constraint) {
+                     $constraint->aspectRatio();
+                })->save(config('image.portfolio_image_path').'/thumbnails/medium/'.$image);
+                        
+                $img->resize(config('image.small_thumbnail_width'), null, function($constraint) {
+                     $constraint->aspectRatio();
+                })->save(config('image.portfolio_image_path').'/thumbnails/small/'.$image);
+        	}
+
+            if(isset($image)){
+                $input['image'] = $image;
+            }
+
+            $portfolioImage = array_intersect_key($input, UserPortfolioImage::$updatable);
+
+            $portfolioImage = UserPortfolioImage::create($portfolioImage);
+
+            if($portfolioImage->save()){
+                return response()->json(['status' => 'success','response' => $portfolioImage]);
+            } else {
+                return response()->json(['status' => 'failure','response' => 'System Error:Portfolio Image could not be added .Please try later.']);
+            }
+        }
     }
 }
