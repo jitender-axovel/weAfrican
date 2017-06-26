f73ff966e392982c89d26f704938d8df4f5d0e0e
jitender-axovel <jitender.axovel@gmail.com>
2017-06-26 10:03:47 -0400

    Created new function to delete old images on update and create new function to update and add product

--- app/BusinessProduct.php
+++ app/BusinessProduct.php
@@ -5,6 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Http\Request;
 use Validator;
+use File;
+use Image;
+use App\BusinessProductImage;
 
 class BusinessProduct extends Model
 {
@@ -48,9 +51,16 @@ class BusinessProduct extends Model
         $input = $request->input();
 
         $validator = Validator::make($input, [
+            'userId' => 'required',
+            'businessId' => 'required',
             'title' => 'required',
             'description' => 'required',
             'price' => 'required',
+            'productImage' => 'sometimes|required|string',
+            'productId' => 'sometimes|required|integer',
+            'updatedImage' => 'sometimes|required|string',
+            'deletedImage' => 'sometimes|required|string',
+            'addedImage' => 'sometimes|required|string',
         ]);
 
         if($validator->fails()){
@@ -61,33 +71,128 @@ class BusinessProduct extends Model
             }
         }
 
-        if(isset($input['productId'])){
-
-            if(isset($input['productImage']) && !empty($input['productImage']))
+        $product = BusinessProduct::whereId($input['productId'])->first();
+        if($product){
+            if(isset($input['updatedImage']) and empty($input['updatedImage']))
             {
-                $data = $input['productImage'];
+                foreach ($input['updatedImage'] as $key => $value) {
+                    if($value!="")
+                    {
+                        $productImage = BusinessProductImage::whereId($key)->first();
+                        if($productImage)
+                        {
+                            if(File::exists(config('image.temp_image_path').$value))
+                            {
+                                if(File::move(config('image.temp_image_path').$value, config('image.product_image_path').$value))
+                                {
+                                    $file = config('image.product_image_path').$value;
 
-                $img = str_replace('data:image/jpeg;base64,', '', $data);
-                $img = str_replace(' ', '+', $img);
+                                    $img = Image::make($file);
+                        
+                                    $img->resize(config('image.large_thumbnail_width'), null, function($constraint) {
+                                         $constraint->aspectRatio();
+                                    })->save(config('image.product_image_path').'/thumbnails/large/'.$value); 
 
-                $data = base64_decode($img);
+                                    $img->resize(config('image.medium_thumbnail_width'), null, function($constraint) {
+                                         $constraint->aspectRatio();
+                                    })->save(config('image.product_image_path').'/thumbnails/medium/'.$value);
+                                            
+                                    $img->resize(config('image.small_thumbnail_width'), null, function($constraint) {
+                                         $constraint->aspectRatio();
+                                    })->save(config('image.product_image_path').'/thumbnails/small/'.$value);
 
-                $fileName = md5(uniqid(rand(), true));
+                                    $oldImg = $product_image->image;
+                                    $product_image->image = $value;
+                                    if(!($product_image->save())){
+                                        $this->deleteProduct($product->id);
+                                        return response()->json(['status' => 'exception','response' => "Image Cannot be saved. Please try again!"]);
+                                    }
+                                    Helper::removeImages(config('image.product_image_path'),$oldImg);
+                                }else
+                                {
+                                    $this->deleteProduct($product->id);
+                                    return response()->json(['status' => 'exception','response' => "Image Cannot be added. Please try again!"]);
+                                }
+                            }
+                        }else
+                        {
+                            return response()->json(['status' => 'exception','response' => 'Product Image not found.Please try again!']);
+                        }
+                    }else
+                    {
+                        return response()->json(['status' => 'exception','response' => 'Image name cannot be blank.']);
+                    }
+                }
+            }
 
-                $image = $fileName.'.'.'png';
+            if(isset($input['deletedImage']) and !empty($input['deletedImage']))
+            {
+                foreach (explode("|", $input['deletedImage']) as $value) {
+                    if($value!="")
+                    {
+                        $productImage = BusinessProductImage::whereId($value)->first();
+                        if($productImage)
+                        {
+                            Helper::removeImages(config('image.product_image_path'),$productImage->image);
+                            $productImage->delete();
+                        }else
+                        {
+                            return response()->json(['status' => 'exception','response' => 'Product Image not found.Please try again!.']);
+                        }
+                    }
+                }
+            }
 
-                $file = config('image.product_image_path').$image;
+            if(isset($input['addedImage']) and !empty($input['addedImage']))
+            {
+                $data = $input['addedImage'];
+                $data = explode("|", $data);
+                if(count($data)>0)
+                {
+                    foreach ($data as $value) {
+                        if(File::exists(config('image.temp_image_path').$value))
+                        {
+                            if(File::move(config('image.temp_image_path').$value, config('image.product_image_path').$value))
+                            {
+                                $file = config('image.product_image_path').$value;
 
-                $success = file_put_contents($file, $data);
+                                $img = Image::make($file);
                     
-                $command = 'ffmpeg -i '.config('image.product_image_path').$image.' -vf scale='.config('image.small_thumbnail_width').':-1 '.config('image.product_image_path').'thumbnails/small/'.$image;
-                shell_exec($command);
+                                $img->resize(config('image.large_thumbnail_width'), null, function($constraint) {
+                                     $constraint->aspectRatio();
+                                })->save(config('image.product_image_path').'/thumbnails/large/'.$value); 
 
-                $command = 'ffmpeg -i '.config('image.product_image_path').$image.' -vf scale='.config('image.medium_thumbnail_width').':-1 '.config('image.product_image_path').'thumbnails/medium/'.$image;
-                shell_exec($command);
+                                $img->resize(config('image.medium_thumbnail_width'), null, function($constraint) {
+                                     $constraint->aspectRatio();
+                                })->save(config('image.product_image_path').'/thumbnails/medium/'.$value);
+                                        
+                                $img->resize(config('image.small_thumbnail_width'), null, function($constraint) {
+                                     $constraint->aspectRatio();
+                                })->save(config('image.product_image_path').'/thumbnails/small/'.$value);
 
-                $command = 'ffmpeg -i '.config('image.product_image_path').$image.' -vf scale='.config('image.large_thumbnail_width').':-1 '.config('image.product_image_path').'thumbnails/large/'.$image;
-                shell_exec($command);
+                                $business_product_image['user_id'] = $input['userId'];
+                                $business_product_image['business_id'] = $input['businessId'];
+                                $business_product_image['business_product_id'] = $product->id;
+                                $business_product_image['featured_image'] = 0;
+                                $business_product_image['image'] = $value;
+
+                                $product_image = array_intersect_key($business_product_image, BusinessProductImage::$updatable);
+                                $product_image = BusinessProductImage::create($product_image);
+                                if(!($product_image->save())){
+                                    return response()->json(['status' => 'exception','response' => "Image Cannot be saved. Please try again!"]);
+                                }
+                            }else
+                            {
+                                $this->deleteProduct($product->id);
+                                return response()->json(['status' => 'exception','response' => "Image Cannot be added. Please try again!"]);
+                            }
+                        }
+                    }
+                    return response()->json(['status' => 'success','response' => "Product Updated successfully."]);
+                }else
+                {
+                     return response()->json(['status' => 'exception','response' => "Atleast one image is required."]);
+                }
             }
 
             $product = array_intersect_key($input, BusinessProduct::$updatable);
@@ -95,54 +200,17 @@ class BusinessProduct extends Model
             $product['user_id'] = $input['userId'];
             $product['id'] = $input['productId'];
 
-            if(isset($image)) {
-                $product['image'] =  $image;
-                $product = BusinessProduct::where('id', $input['productId'])->where('user_id', $input['userId'])->update($product);
-            } else {
-                $product = BusinessProduct::where('id', $input['productId'])->where('user_id', $input['userId'])->update($product);
-            }
+            $product = BusinessProduct::where('id', $input['productId'])->where('user_id', $input['userId'])->update($product);
 
             if($product)
                 return response()->json(['status' => 'success','response' => "Product updated successfully."]);
             else
                 return response()->json(['status' => 'failure','response' => "Product could not updated successfully.Please try again."]);
         }else{
-             
-            if(isset($input['productImage']) && !empty($input['productImage']))
-            {
-                $data = $input['productImage'];
-
-                $img = str_replace('data:image/jpeg;base64,', '', $data);
-                $img = str_replace(' ', '+', $img);
-
-                $data = base64_decode($img);
-
-                $fileName = md5(uniqid(rand(), true));
-
-                $image = $fileName.'.'.'png';
-
-                $file = config('image.product_image_path').$image;
-
-                $success = file_put_contents($file, $data);
-                    
-                $command = 'ffmpeg -i '.config('image.product_image_path').$image.' -vf scale='.config('image.small_thumbnail_width').':-1 '.config('image.product_image_path').'thumbnails/small/'.$image;
-                shell_exec($command);
-
-                $command = 'ffmpeg -i '.config('image.product_image_path').$image.' -vf scale='.config('image.medium_thumbnail_width').':-1 '.config('image.product_image_path').'thumbnails/medium/'.$image;
-                shell_exec($command);
-
-                $command = 'ffmpeg -i '.config('image.product_image_path').$image.' -vf scale='.config('image.large_thumbnail_width').':-1 '.config('image.product_image_path').'thumbnails/large/'.$image;
-                shell_exec($command);
-            }
-
 
             $product = array_intersect_key($request->input(), BusinessProduct::$updatable);
             $product['user_id'] = $input['userId'];
             $product['business_id'] = $input['businessId'];
-
-            if(isset($image)){
-                $product['image'] = $image;
-            }
 
             $product = BusinessProduct::create($product);
             $product->save();
@@ -150,10 +218,79 @@ class BusinessProduct extends Model
             $product->slug = Helper::slug($product->title, $product->id);
 
             if($product->save()){
+                if(isset($input['productImage']) && !empty($input['productImage']))
+                {
+                    $data = $input['productImage'];
+                    $data = explode("|", $data);
+                    if(count($data)>0)
+                    {
+                        foreach ($data as $value) {
+                            if(File::exists(config('image.temp_image_path').$value))
+                            {
+                                if(File::move(config('image.temp_image_path').$value, config('image.product_image_path').$value))
+                                {
+                                    $file = config('image.product_image_path').$value;
+
+                                    $img = Image::make($file);
+                        
+                                    $img->resize(config('image.large_thumbnail_width'), null, function($constraint) {
+                                         $constraint->aspectRatio();
+                                    })->save(config('image.product_image_path').'/thumbnails/large/'.$value); 
+
+                                    $img->resize(config('image.medium_thumbnail_width'), null, function($constraint) {
+                                         $constraint->aspectRatio();
+                                    })->save(config('image.product_image_path').'/thumbnails/medium/'.$value);
+                                            
+                                    $img->resize(config('image.small_thumbnail_width'), null, function($constraint) {
+                                         $constraint->aspectRatio();
+                                    })->save(config('image.product_image_path').'/thumbnails/small/'.$value);
+
+                                    $business_product_image['user_id'] = $input['userId'];
+                                    $business_product_image['business_id'] = $input['businessId'];
+                                    $business_product_image['business_product_id'] = $product->id;
+                                    $business_product_image['featured_image'] = 0;
+                                    $business_product_image['image'] = $value;
+
+                                    $product_image = array_intersect_key($business_product_image, BusinessProductImage::$updatable);
+                                    $product_image = BusinessProductImage::create($product_image);
+                                    if(!($product_image->save())){
+                                        $this->deleteProduct($product->id);
+                                        return response()->json(['status' => 'exception','response' => "Image Cannot be saved. Please try again!"]);
+                                    }
+
+                                }else
+                                {
+                                    $this->deleteProduct($product->id);
+                                    return response()->json(['status' => 'exception','response' => "Image Cannot be added. Please try again!"]);
+                                }
+                            }
+                        }
+                        return response()->json(['status' => 'success','response' => "Product added successfully."]);
+                    }else
+                    {
+                         return response()->json(['status' => 'exception','response' => "Atleast one image is required."]);
+                    }
+                }else
+                {
+                     return response()->json(['status' => 'exception','response' => "Atleast one image is required."]);
+                }
                 return response()->json(['status' => 'success','response' => $product]);
             } else {
                 return response()->json(['status' => 'failure','response' => 'System Error:Product could not be created .Please try later.']);
             }
+        }
+    }
+
+    public function deleteProduct($id)
+    {
+        $product = BusinessProduct::whereId($id)->first();
+        if($product)
+        {
+            $product_images = BusinessProductImage::where('business_product_id',$id)->get();
+            foreach ($product_images as $key => $value) {
+                Helper::removeImages(config('image.product_image_path'),$value);
+            }
+            $product->delete();
         }
     }
 }
